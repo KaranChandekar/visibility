d1a359854158ff5feb0d0727b3e35db4199cd787
Karan <karanchandekar431@gmail.com>
2023-01-19 00:28:40 +0530

    update navbar using tailwind

--- js/script.js
+++ js/script.js
@@ -5,7 +5,6 @@ const navLinks = document.getElementById("nav-links");
 
 const onToggleMenu = (e) => {
   e.name = e.name === "menu" ? "close" : "menu";
-
-  navLinks.classList.toggle("opacity-0");
-  navLinks.classList.toggle("top-[-400px]");
+  navLinks.classList.toggle("translate-x-0");
+  navLinks.classList.toggle("translate-x-[-100%]");
 };
