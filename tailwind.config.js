69a3d2113d26f0fbe2cafd831be2d56ace228b7f
Karan <karanchandekar431@gmail.com>
2023-01-19 15:47:22 +0530

    added contact bg image

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,7 @@ module.exports = {
         "bg-home": "url('/images/home-bg.svg')",
         "bg-about": "url('/images/about-blur.svg')",
         "bg-case-studies": "url('/images/case-study.png')",
+        "bg-contact": "url('/images/contact-glow.944a9bf8.svg')",
       },
     },
   },
