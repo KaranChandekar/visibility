6fd0582d5f718267bd7071c02fb48747dda6f911
Karan <karanchandekar431@gmail.com>
2023-01-22 09:27:28 +0530

    update images for contact page

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,7 @@ module.exports = {
     extend: {
       backgroundImage: {
         "bg-img": "url('/images/home-bg.svg')",
-        "card-img": "url('/images/card-red-glow.303cd894.svg')",
+        "card-img": "url('/images/glow/card-red-glow.303cd894.svg')",
         // "bg-img": "url('/images/about-blur.svg')",
         // "bg-img": "url('/images/case-study.png')",
         // "bg-img": "url('/images/contact-glow.944a9bf8.svg')",
